047c6907f39b43c1625ff0ee9d6b08fb3b1d9915
Shourov Paul <101161294+Shourov0@users.noreply.github.com>
2024-03-21 11:05:44 +0600

    Update Volume Controller knob.ino

--- Volume Controller knob/Volume Controller knob.ino
+++ Volume Controller knob/Volume Controller knob.ino
@@ -1,48 +1,26 @@
-#include <Adafruit_NeoPixel.h>
-#include <RotaryEncoder.h>
 #include <Keyboard.h>
+#include <RotaryEncoder.h>
+#include <Adafruit_NeoPixel.h>
 
-#define PIXELPIN   22
+#define PIXELPIN   13
 #define NUMPIXELS  8
-#define MUTE_PIN   18 
-#define ENCODERA   16
-#define ENCODERB   17
+#define MUTE_PIN   10
 
+#define PIN_IN1 14
+#define PIN_IN2 15
+
+#define ROTARYSTEPS 2
+#define ROTARYMIN 0
+#define ROTARYMAX 100
+
+
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
 Adafruit_NeoPixel pixels(NUMPIXELS, PIXELPIN, NEO_GRB + NEO_KHZ800);
-RotaryEncoder encoder(ENCODERA, ENCODERB);
-int lastVolume = 0;
-int neopixelPosition = 0;
 
-void setup() {
-  pixels.begin();
-  pixels.setBrightness(5); // Adjust brightness if needed
-  
-  pinMode(MUTE_PIN, INPUT_PULLUP);
-  Keyboard.begin();
-  //Serial.begin(115200);
-  for (int volume = 100; volume >= 0; volume--) {
-    volumeCalebreation(volume);
-    delay(10); // Adjust delay as needed
-  }
-}
+// Last known rotary position.
+int lastPos = -1;
 
-void loop() {
-  encoder.tick();
-
-  adjustVolume(encoder.getPosition()/2);
-  //Serial.print(encoder.getPosition());
-  
-  muteUnmute();
-  //Serial.print(" || ");
-  
-  int position = map(encoder.getPosition(), 0, 100, 0, NUMPIXELS);
-  //position = constrain(position, 0, 100); // Limit position within range
-  //Serial.println(position);
-
-  updateNeopixels(position);
-}
-
-void volumeCalebreation(int volume) {
+void volumeCalibration(int volume) {
   switch (volume) {
     case 100:
       Keyboard.consumerPress(KEY_MUTE);
@@ -52,43 +30,6 @@ void volumeCalebreation(int volume) {
       break;
   }
 }
-
-void adjustVolume(int position) {
-  // If position is negative, reset to zero
-  if (position < 0) {
-    encoder.setPosition(0);
-    position = 0;
-  }
-  
-  // If position exceeds 100, reset to 100
-  if (position > 100) {
-    encoder.setPosition(100);
-    position = 100;
-  }
-
-  int volumeDelta = position - lastVolume;
-
-  if (volumeDelta != 0) {
-    if (volumeDelta > 0) {
-      for (int i = 0; i < volumeDelta; i++) {
-        Keyboard.consumerPress(KEY_VOLUME_INCREMENT);
-        delay(50);
-        Keyboard.consumerRelease();
-      }
-    } else {
-      for (int i = 0; i > volumeDelta; i--) {
-        Keyboard.consumerPress(KEY_VOLUME_DECREMENT);
-        delay(50);
-        Keyboard.consumerRelease();
-      }
-    }
-    lastVolume = position;
-  }
-}
-
-
-
-
 void muteUnmute() {
   int btn = digitalRead(MUTE_PIN);
   if (btn == LOW) {
@@ -100,7 +41,6 @@ void muteUnmute() {
     Keyboard.consumerRelease();
   }
 }
-
 void flashNeopixels() {
   // Turn on all Neopixels
   for (int i = 0; i < NUMPIXELS; i++) {
@@ -116,17 +56,76 @@ void flashNeopixels() {
   }
   pixels.show();
 }
-
-
-void updateNeopixels(int position) {//This gives the best result
+void updateNeopixels(int pos) {//This gives the best result
+  int position = map(pos , ROTARYMIN ,ROTARYMAX , 0 , NUMPIXELS);
   for (int i = 0; i < NUMPIXELS; i++) {
     if (position == 0) {
       pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Turn off all pixels
     } else if (i < position) {
-      pixels.setPixelColor(i, pixels.Color(10, 255, 12)); // White color, you can adjust RGB values
+      pixels.setPixelColor(i, pixels.Color(10, 255, 12)); // you can adjust RGB values
     } else {
       pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Turn off pixel
     }
   }
   pixels.show();
+}
+void setup()
+{
+  //Serial.begin(115200);
+  pinMode(MUTE_PIN, INPUT_PULLUP);
+  pixels.begin();
+  pixels.setBrightness(20);
+  Keyboard.begin();
+  //while (! Serial);
+  //Serial.println("LimitedRotator example for the RotaryEncoder library.");
+  encoder.setPosition(0); // start with the value of 0.
+  for (int volume = 100; volume >= 0; volume--) { //Reduce volume to zero to match the encoder's position
+    volumeCalibration(volume);
+    delay(10); 
+  }
+}
+
+
+// Read the current position of the encoder and print out when changed.
+void loop()
+{
+  encoder.tick();
+  muteUnmute();
+
+  // get the current physical position and calc the logical position
+  int newPos = encoder.getPosition() * ROTARYSTEPS;
+
+  if (newPos < ROTARYMIN) {
+    encoder.setPosition(ROTARYMIN / ROTARYSTEPS);
+    newPos = ROTARYMIN;
+
+  } else if (newPos > ROTARYMAX) {
+    encoder.setPosition(ROTARYMAX / ROTARYSTEPS);
+    newPos = ROTARYMAX;
+  } // if
+
+  if (lastPos != newPos) {
+    //Serial.print(newPos);
+    //Serial.println();
+    
+  int volumeDelta = newPos - lastPos;
+  
+  if (volumeDelta != 0) {
+    if (volumeDelta > 0) {
+      for (int i = 0; i < volumeDelta; i++) {
+        Keyboard.consumerPress(KEY_VOLUME_INCREMENT);
+        delay(10);
+        Keyboard.consumerRelease();
+      }
+    } else {
+      for (int i = 0; i > volumeDelta; i--) {
+        Keyboard.consumerPress(KEY_VOLUME_DECREMENT);
+        delay(20);
+        Keyboard.consumerRelease();
+      }
+    }
+    updateNeopixels(newPos);
+    lastPos = newPos;
+   }
+  }
 }
